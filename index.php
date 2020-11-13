4bbb878c737cea9b1463537576d12a42bf2cc137
Irvin753 <74386795+Irvin753@users.noreply.github.com>
2020-11-12 17:23:41 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<!--
+	Fox by FreeHTML5.co
+	Twitter: https://twitter.com/fh5co
+	Facebook: https://fb.com/fh5co
+	URL: https://freehtml5.co
+-->
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>SoccerLeagueEscuinapa</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="css/animate.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container-fluid pl-0 pr-0 bg-img clearfix parallax-window2" data-parallax="scroll" data-image-src="images/banner2.jpg">
+  <nav class="navbar navbar-expand-md navbar-dark">
+    <div class="container"> 
+      <!-- Brand --> 
+      <a class="navbar-brand mr-auto" href="#"><img src="images/logo.png" alt="SoccerLeagueEscuinapa" /></a> 
+      
+      <!-- Toggler/collapsibe Button -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> <span class="navbar-toggler-icon"></span> </button>
+      
+      <!-- Navbar links -->
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"> <a class="nav-link" href="#">Inicio</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#"> Equipos</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#about-us"></a> </li>
+        </ul>
+        <ul class="navbar-nav ml-5">
+          <li class="nav-item"> <a class="nav-link btn btn-danger" href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="fh5co-banner-text-box">
+      <div class="quote-box pl-5 pr-5 wow bounceInRight">
+        <h2> SoccerLeagueEscuinapa <br> </h2>
+      </div>
+      <a href="#" class="btn text-uppercase btn-outline-danger btn-lg mr-3 mb-3 wow bounceInUp"> Bienvenido</a> 
+      <a href="#" class="btn text-uppercase btn-outline-danger btn-lg mb-3 wow bounceInDown"> La mejor pagina </a> </div>
+  </div>
+</div>
+<div class="container-fluid fh5co-network">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h2 class="wow bounceInRight">MIEMBROS</h2>
+        <hr />
+        <h5 class="wow bounceInLeft">2020</h5>
+        <p class="wow bounceInDown">Irvin Toledo Garcia y Miguel Angel Diaz Arrañaga</p>
+      </div>
+      <div class="col-md-6">
+        <figure class="wow bounceInDown"> <img src="images/about-img.jpg"  class="img-fluid" /> </figure>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="about-us" class="container-fluid fh5co-about-us pl-0 pr-0 parallax-window" data-parallax="scroll" data-image-src="images/about-us-bg.jpg">
+  <div class="container">
+    <div class="col-sm-6 offset-sm-6">
+      <h2 class="wow bounceInLeft" data-wow-delay=".25s">Acerca de nosotros</h2>
+      <hr/>
+      <p class="wow bounceInRight" data-wow-delay=".25s"> Somos alumnos de V-B de la especialidad de programación de la institucion de Cbtis 152. Toledo Garcia Irvin y Miguel Angel Diaz Arrañaga, esta pagina fue creada
+        en 2020.
+      </p>
+  </div>
+</div>
+<div class="container-fluid fh5co-content-box">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5 pr-0"><img src="images/cbtis.png"  class="img-fluid wow bounceInLeft" /> </div>
+      <div class="col-md-7 pl-0">
+        <div class="wow bounceInRight" data-wow-delay=".25s">
+          <div class="card-img-overlay">
+       
+          </div>
+          <img src="images/rode-gym.jpg"   class="img-fluid" /> </div>
+      </div>
+    </div>
+    <div class="row trainers pl-0 pr-0">
+      <div class="col-12 bg-50">
+        <div class="quote-box2 wow bounceInDown" data-wow-delay=".25s">
+          <h2> Creadores de la pagina web </h2>
+        </div>
+      </div>
+      <div class="col-md-6 pr-5 pl-5">
+        <div class="card text-center wow bounceInLeft" data-wow-delay=".25s"> <img class="card-img-top rounded-circle img-fluid" src="images/miguel2.png" alt="Card image">
+          <div class="card-body mb-5">
+            <h4 class="card-title">Miguel Diaz</h4>
+            <p class="card-text">Este es un texto de relleno escrito manualmente</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 pl-5 pr-5">
+        <div class="card text-center wow bounceInLeft" data-wow-delay=".25s"> <img class="card-img-top rounded-circle img-fluid" src="images/irvin.png" alt="Card image">
+          <div class="card-body mb-5">
+            <h4 class="card-title">Irvin Toledo</h4>
+            <p class="card-text">Este es un texto de relleno escrito manualmente</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<footer class="container-fluid">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 footer1 d-flex wow bounceInLeft" data-wow-delay=".25s">
+        <div class="d-flex flex-wrap align-content-center"> <a href="#"><img src="images/logo.png" alt="logo" /></a>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+            dolor. Ae</p>
+          <p>Irvin Toledo Garcia y Miguel Angel Diaz Arrañaga (2020).</p>
+        </div>
+      </div>
+      <div class="col-md-6 footer2 wow bounceInUp" data-wow-delay=".25s" id="contact">
+        <div class="form-box">
+          <h4>CONTACTANOS</h4>
+          <table class="table table-responsive d-table">
+            <tr>
+              <td><input type="text" class="form-control pl-0" placeholder="NOMBRE" /></td>
+              <td><input type="email" class="form-control pl-0" placeholder="CORREO ELECTRONICO" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+            </tr>
+            <tr>
+              <td colspan="2"><input type="text" class="form-control pl-0" placeholder="MENSAJE PARA:" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+            </tr>
+            <tr>
+              <td colspan="2"><input type="text" class="form-control pl-0" placeholder="MENSAJE" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"></td>
+            </tr>
+            <tr>
+              <td colspan="2" class="text-center pl-0"><button type="submit" class="btn btn-dark">ENVIAR</button></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div class="col-md-3 footer3 wow bounceInRight" data-wow-delay=".25s">
+        <h5>PHONE</h5>
+        <p>253232323232</p>
+        <h5>CORREO ELECTRONICO</h5>
+        <p>email@example.com</p>
+        
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- Optional JavaScript --> 
+<!-- jQuery first, then Bootstrap JS, ... -->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/parallax.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
